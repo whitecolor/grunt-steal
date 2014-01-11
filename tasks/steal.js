3e65de9b48e52020ce593adb3d17d2975015e7d7
Alex Oshchepkov <alex.oschepkov@gmail.com>
2014-01-11 06:44:24 +0600

    Path and arguments

--- tasks/steal.js
+++ tasks/steal.js
@@ -1,77 +1,83 @@
 module.exports = function(grunt) {
-  grunt.registerTask('steal', 'Build your application with StealJS', function() {
-    this.requiresConfig('steal');
+    grunt.registerTask('steal', 'Build your application with StealJS', function() {
+        this.requiresConfig('steal');
 
-    var steal = grunt.config('steal'),
-    done = this.async(),
-    promise = require('promised-io/promise'),
 
-    build = steal.build && steal.build.length ? steal.build : [],
-    js = require('os').platform() === 'win32' ? 'js.bat' : './js',
+        var steal = grunt.config('steal'),
+            done = this.async(),
+            promise = require('promised-io/promise'),
 
-    gruntDir = process.cwd(),
-    instances = [],
 
-    runSteal = function(args) {
-      var deferred = new promise.Deferred();
-      grunt.log.writeln('\nRunning: ' + js + ' ' + args.join(' '));
 
-      grunt.util.spawn({
-        cmd: js,
-        args: args
-      }, function(e, result, code) {
-        if(code) {
-          deferred.reject(e);
-        }
-        else {
-          grunt.log.write(result.stdout);
-          deferred.resolve();
-        }
-      });
+        // added this.args
+            build = steal[this.args[0]] || (steal.build && steal.build.length ? steal.build : []),
+            js = require('os').platform() === 'win32' ? 'js.bat' : './js',
 
-      return deferred.promise;
-    };
 
-    process.chdir(steal.js || '.');
+            gruntDir = process.cwd(),
+            instances = [],
 
-    var execute = function(i) {
-      var opts = typeof build[i] === 'string' ? {
-        src: build[i]
-      } : build[i],
-      args = [];
+            runSteal = function(args) {
+                var deferred = new promise.Deferred();
+                grunt.log.writeln('\nRunning: ' + js + ' ' + args.join(' '));
 
-      args.push(opts.src);
-      delete opts.src;
+                grunt.util.spawn({
+                    cmd: (steal.js || '.') + '/' + js,
+                    args: args
+                }, function(e, result, code) {
+                    if(code) {
+                        deferred.reject(e);
+                    }
+                    else {
+                        grunt.log.write(result.stdout);
+                        deferred.resolve();
+                    }
+                });
 
-      for(var name in opts) {
-        if(opts[name]) {
-          args.push('-' + name);
+                return deferred.promise;
+            };
 
-          if(typeof opts[name] !== 'boolean') {
-            args.push(opts[name]);
-          }
-        }
-      }
 
-      var deferred = runSteal(args);
+        //process.chdir(steal.js || '.');
 
-      deferred.then(function() {
-        if(i < build.length - 1) {
-          execute(++i);
-        }
-        else {
-          process.chdir(gruntDir);
+        var execute = function(i) {
+            var opts = typeof build[i] === 'string' ? {
+                    src: build[i]
+                } : build[i],
+                args = [];
 
-          grunt.log.ok();
-          done();
-        }
-      }, function(e) {
-        grunt.log.error(e.stdout);
+            args.push(opts.src);
+            delete opts.src;
 
-        done(false);
-      });
-    };
+            for(var name in opts) {
+                if(opts[name]) {
+                    args.push('-' + name);
 
-    execute(0);
-  });
+                    if(typeof opts[name] !== 'boolean') {
+                        args.push(opts[name]);
+                    }
+                }
+            }
+
+            var deferred = runSteal(args);
+
+            deferred.then(function() {
+                if(i < build.length - 1) {
+                    execute(++i);
+                }
+                else {
+                    process.chdir(gruntDir);
+
+                    grunt.log.ok();
+                    done();
+                }
+            }, function(e) {
+                grunt.log.error(e.stdout);
+
+                done(false);
+            });
+        };
+
+        execute(0);
+    });
 };
